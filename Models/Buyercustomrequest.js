5a0cc179d2d4d94723b982ff392fcc48a8f41dc0
Shantanu Belsare <shantanubelsare13@gmail.com>
2025-06-30 10:13:28 +0530

    BuyerNego

--- Models/Buyercustomrequest.js
+++ Models/Buyercustomrequest.js
@@ -29,7 +29,59 @@
 // module.exports = mongoose.model("BuyerRequest", BuyerRequestSchema);
 
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const BuyerRequestSchema = new mongoose.Schema(
+//   {
+//     ProductName: { type: String, required: true },
+//     Description: { type: String, required: true },
+//     BuyerImage: { type: String, required: true },
+//     Buyer: {
+//       id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//       name: { type: String },
+//       email: { type: String }
+//     },
+//     Artist: {
+//       id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//       name: { type: String }
+//     },
+//     ArtType: { type: String, required: true },
+//     Size: { type: String, required: true },
+//     ColourPreferences: { type: [String], required: true },
+//     IsFramed: { type: Boolean, required: true },
+//     MinBudget: { type: Number, required: true },
+//     MaxBudget: { type: Number, required: true },
+//     PaymentTerm: { type: String, required: true },
+//     ExpectedDeadline: { type: Number, required: true },
+//     Comments: { type: String },
+//     RequestStatus: {
+//       type: String,
+//       enum: ['Approved', 'Rejected', 'Pending'],
+//       default: 'Pending',
+//     },
+//     BuyerStatus: {
+//       type: String,
+//       enum: ['Approved', 'Rejected', 'Pending'],
+//       default: 'Pending',
+//     },
+//     rejectedcomment: { type: String },
+//     NegotiatedBudget: [{ type: Number }],
+//     Notes: { type: String, required: false },
+//     BuyerNotes: { type: String },
+//     updateCount: { type: Number, default: 0 },
+//     isUpdated: { type: Number, default: 0 },
+//     Revisions: [{
+//       description: String,
+//       date: { type: Date, default: Date.now }
+//     }]
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("BuyerRequest", BuyerRequestSchema);
+
+
+const mongoose = require('mongoose');
 
 const BuyerRequestSchema = new mongoose.Schema(
   {
@@ -65,7 +117,8 @@ const BuyerRequestSchema = new mongoose.Schema(
       default: 'Pending',
     },
     rejectedcomment: { type: String },
-    NegotiatedBudget: [{ type: Number }],
+    BuyerNegotiatedBudgets: [{ type: Number, default: [] }],
+    ArtistNegotiatedBudgets: [{ type: Number, default: [] }],
     Notes: { type: String, required: false },
     BuyerNotes: { type: String },
     updateCount: { type: Number, default: 0 },
